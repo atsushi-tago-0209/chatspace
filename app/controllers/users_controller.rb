b0ea81a29e86b201ae1312ab3f86e8ec912eae55
atsushi-tago-0209 <0209at@gmail.com>
2020-01-20 21:08:56 +0900

    sousin finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,20 +2,20 @@ class UsersController < ApplicationController
 def edit
 end
 
-def upadate
-  if current_user.update(user_params)
-    redirect_to root_path
-  else
-    render :edit
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
-end
 
-def create
-end
+  def create
+  end
 
-private
+  private
 
-def user_params
-  params.require(:user).permit(:name, :email)
-end
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
