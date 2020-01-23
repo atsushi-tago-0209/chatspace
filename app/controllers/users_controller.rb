59dc2db801948bffe0db18733488321a894d71ba
atsushi-tago-0209 <0209at@gmail.com>
2020-01-23 15:28:30 +0900

    inclimental search finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
-def edit
+  	
+def index
+  return nil if params[:keyword] == ""
+  @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+  respond_to do |format|
+    format.html
+    format.json
+  end
 end
+
+  def edit
+  end
 
   def update
     if current_user.update(user_params)
@@ -15,5 +25,5 @@ end
   def user_params
     params.require(:user).permit(:name, :email)
   end
-
 end
+
